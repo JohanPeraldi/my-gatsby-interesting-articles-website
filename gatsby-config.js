0190251dc03a91bca5397a90f4ba5496dd88c73a
Johan <73821712+JohanPeraldi@users.noreply.github.com>
2025-03-21 18:53:24 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Interesting Articles Website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
